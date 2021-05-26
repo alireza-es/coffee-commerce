1f27848098fee7b470902ac27213d52be7e3e87f
Alireza <alireza.es@gmail.com>
2021-05-27 01:57:32 +0430

    Finalize code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheService, TypeOrmService } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmService
+    }),
+    CacheModule.registerAsync({
+      useClass: CacheService
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/environments/index.ts
@@ -0,0 +1,41 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// environment
+const NODE_ENV: string = process.env.NODE_ENV || 'development';
+
+// application
+const PORT: number = +process.env.PORT || 3000;
+const DOMAIN: string = process.env.DOMAIN || 'localhost';
+// MongoDB
+const ATLAS_DB_USER = process.env.ATLAS_USER || 'admin'
+const ATLAS_DB_PASS = process.env.ATLAS_PASS || 'bYBQuQybB3qHsy3L'
+const ATLAS_DB_HOST = process.env.ATLAS_HOST || 'cluster0.1ot9b.azure.mongodb.net'
+const ATLAS_DB_DATABASE = process.env.ATLAS_DATABASE || 'booking'
+const ATLAS_DB_URL = process.env.ATLAS_DB_URL || `mongodb+srv://${ATLAS_DB_USER}:${ATLAS_DB_PASS}@${ATLAS_DB_HOST}/${ATLAS_DB_DATABASE}?retryWrites=true&w=majority`;
+
+// typeorm
+const environment = {
+	development: {
+		url: ATLAS_DB_URL
+	},
+	testing: {
+		url: ATLAS_DB_URL
+	},
+	staging: {
+		url: ATLAS_DB_URL
+	},
+	production: {
+		url: ATLAS_DB_URL
+	}
+};
+const TYPE_ORM = environment[NODE_ENV];
+
+export {
+	NODE_ENV,
+	DOMAIN,
+	PORT,
+	TYPE_ORM,
+	ATLAS_DB_URL
+};
+
